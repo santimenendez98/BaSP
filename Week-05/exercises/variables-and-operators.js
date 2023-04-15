94b76016226013540ea2f2356a0133368a2bd2e8
santimenendez98 <santi.mm11@gmail.com>
2023-04-15 13:39:15 -0300

    BaSP-W05-2023: Added files for week05 directory and finished the variables-and-operators exercise

---
+++ Week-05/exercises/variables-and-operators.js
@@ -0,0 +1,29 @@
+// Ejercicio 1
+
+var number1 = 12;
+
+var number2 = 24;
+
+var sum = number1 + number2
+
+console.log('Ejercicio 1a =', sum)
+
+// Ejercicio 2
+
+var word1 = 'Santiago ';
+
+var word2 = 'Menendez';
+
+var concatenatedStrings = palabra1 + palabra2;
+
+console.log('Ejercicio 1b =', concatenatedStrings)
+
+// Ejercicio 3
+
+var string1 = 'palabra1';
+
+var string2 = 'palabra2';
+
+var numberOfLetters = word1.length + word2.length;
+
+console.log('Ejercicio 1c =', numberOfLetters)
