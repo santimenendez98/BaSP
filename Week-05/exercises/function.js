ead9cefb2f78272fc463b7d33aefaad1dae7f0b3
santimenendez98 <santi.mm11@gmail.com>
2023-04-18 15:11:06 -0300

    BaSP-W05-2023: Function

--- Week-05/exercises/function.js
+++ Week-05/exercises/function.js
@@ -1,63 +1,74 @@
 // Ejercicio 1
 
-function sum (a,b) {
-    return a + b
-};
+function sum(a, b) {
+  return a + b;
+}
 
-var resultSum = sum(2,3);
+var resultSum = sum(2, 3);
 
-console.log('Ejercicio 6a =', resultSum)
+console.log("Ejercicio 6a =", resultSum);
 
 // Ejercicio 2
 
-function suma (a,b) {
-    if(typeof a !== 'number' || typeof b !== 'number'){
-        alert('Los parametros tienen que ser tipo numero');
-        return NaN;
-    }
-    return a + b
-};
+function suma(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return NaN;
+  }
+  return a + b;
+}
 
-console.log('Ejercicio 6b =', sum(2,'Funciones'))
+console.log("Ejercicio 6b =", suma(2, "Funciones"));
 
 // Ejercicio 3
 
-function validateInteger (number) {
-    var numberVerification = number % 1;
-    if(numberVerification === 0) {
-        return true;
-    } else {
-        return false
-    }
-};
+function validateInteger(number) {
+  var numberVerification = number % 1;
+  if (numberVerification === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-console.log('Ejercicio 6c =', validateInteger(3))
+console.log("Ejercicio 6c =", validateInteger(3));
 
 // Ejercicio 4
 
-function sumValidation (a,b) {
-    if(!validateInteger(a) || !validateInteger(b)){
-        if(a === true && b === false){
-            b = Math.round(b);
-            alert("El segundo número no es un número entero. Se redondeará a " + b);
-        } else if(a === false && b === true){
-            a = Math.round(a);
-            alert("El primer número no es un número entero. Se redondeará a " + a);
-        } else {
-            a = Math.round(a);
-            b = Math.round(b);
-            alert("Ambos números no son enteros. Se redondearán a " + a + " y " + b);
-        }
-    }
-    return a + b;
-};
+function sumValidation(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    alert('A value is invalid');
+    return NaN;
+  } else if (!validateInteger(a) || !validateInteger(b)) {
+    alert('A value is not a integer')
+    a = Math.round(a);
+    b = Math.round(b);
+  }
+  return a + b;
+}
 
-console.log('Ejercicio 6d =', sumValidation(3.2,3.4))
+console.log("Ejercicio 6d =", sumValidation(3.6, 2));
 
 //Ejercicio 5
 
-function newValidation (a,b){
-    return sumValidation(a,b)
-};
+function validationNum(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    alert('A value is invalid');
+    return NaN;
+  } else if (!validateInteger(a) || !validateInteger(b)) {
+    alert('A value is not integer');
+    a = Math.round(a);
+    b = Math.round(b);
+  }
+  return a + b;
+}
 
-console.log('Ejercicio 6e =', newValidation(2.3,4.2))
+function validation(value1, value2) {
+  var validate = validationNum(value1, value2);
+  if (typeof validate === "number") {
+    return validationNum(value1, value2);
+  } else {
+    return value1 + value2;
+  }
+}
+
+console.log("Ejercicio 6e =", validation(2.3,'2'));
